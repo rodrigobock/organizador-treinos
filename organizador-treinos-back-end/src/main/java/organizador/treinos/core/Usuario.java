1744a0a09b9ba76b26e37057b3ecf8b28167916d
Rodrigo Cabral Bock <rodrigocabralbock@gmail.com>
2024-03-28 09:37:44 -0300

    adicionado o lombok ao código

---
+++ organizador-treinos-back-end/src/main/java/organizador/treinos/core/Usuario.java
@@ -0,0 +1,13 @@
+package organizador.treinos.core;
+
+import lombok.Data;
+
+@Data
+public class Usuario {
+
+    private int idUsuario;
+    private String usuario;
+    private String senha;
+
+}
+
